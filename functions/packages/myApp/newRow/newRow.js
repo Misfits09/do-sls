7059ab6e9c4ba0e9ef7d72684508dceffc0e1151
Nathan Perriolat <nathanp@theodo.fr>
2022-06-30 16:45:02 +0200

    show env

--- functions/packages/myApp/newRow/newRow.js
+++ functions/packages/myApp/newRow/newRow.js
@@ -1,6 +1,6 @@
 function main(args) {
     let name = args.name || 'stranger'
-    let greeting = `Hello ${name} !`
+    let greeting = `Hello ${name} ! ${process.env.DB_HOSTNAME}`
     console.log(greeting)
     return {body: greeting}
   }
