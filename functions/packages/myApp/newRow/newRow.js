b2f9ddcec1baa1c697c63c30271257430b2c461e
Nathan Perriolat <nathanp@theodo.fr>
2022-06-30 16:55:21 +0200

    psql connection attempt

--- functions/packages/myApp/newRow/newRow.js
+++ functions/packages/myApp/newRow/newRow.js
@@ -1,8 +1,21 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = `Hello ${name} ! ${process.env.DB_HOSTNAME}`
-    console.log(greeting)
-    return {body: greeting}
-  }
+const { Client } = require("pg");
+const client = new Client({
+  user: "username",
+  host: process.env.DB_HOSTNAME,
+  database: "myapp",
+  password: "p@ssw0rd",
+  port: 5432,
+});
+
+async function main(args) {
+  let name = args.name || "stranger";
+  let greeting = `Hello ${name} ! ${process.env.DB_HOSTNAME}`;
+  console.log(greeting);
+  await client.connect();
+  const res = await client.query("INSERT INTO data VALUES (111,?)",[name])
+  console.debug(res);
+
+  return { body: greeting };
+}
 
 module.exports.main = main;
