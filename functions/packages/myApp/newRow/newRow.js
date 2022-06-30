dc18da22b1b86cfe90c4f452b4ace03abd324d21
Nathan Perriolat <nathanp@theodo.fr>
2022-06-30 17:41:29 +0200

    add logging CA

--- functions/packages/myApp/newRow/newRow.js
+++ functions/packages/myApp/newRow/newRow.js
@@ -2,7 +2,6 @@ const { Client } = require("pg");
 const client = new Client({
   connectionString: process.env.DB_URL,
   ssl: {
-    rejectUnauthorized: false,
     ca: process.env.DB_CA
   }
 });
@@ -13,12 +12,16 @@ async function main(args) {
   try{
     console.log(greeting);
     console.log("Attempting connection...")
+    console.log("Using CA :", process.env.DB_CA)
     
     console.log("Done ? ", await client.connect())
     const res = await client.query("INSERT INTO data VALUES (111,?)",[name])
     console.log("Insert response:" , res);
   }catch(e){
     console.log("Error happened", e);
+    try{
+      client.end()
+    }catch(e){}
     return {body: "Unhappy !"}
   }
   
